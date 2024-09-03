c6db23d0ffa5710bbd8bd3e4d58b8933161d4e2b
LucasRSteimetz <steimetz.lucas@gmail.com>
2024-09-03 16:38:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import { PNG } from 'pngjs';
+import pixelmatch from 'pixelmatch';
+
+const img1 = PNG.sync.read(fs.readFileSync('image1.png'));
+const img2 = PNG.sync.read(fs.readFileSync('image2.png'));
+const {width, height} = img1;
+const diff = new PNG({width, height});
+
+pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0.1});
+
+fs.writeFileSync('diff.png', PNG.sync.write(diff));
